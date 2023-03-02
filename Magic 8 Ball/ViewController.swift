9f90d879bc6cca3a69ba44645f665dbf16c8db31
Betty Jimenez <bettyjimenez3010@gmail.com>
2023-03-02 15:50:39 -0400

    connect for generate random

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var imageBallAn: UIImageView!
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    @IBAction func handleBall(_ sender: UIButton) {
+        let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+        imageBallAn.image = ballArray[Int.random(in: 0...4)]
+        print("hola")
+    }
 }
 
